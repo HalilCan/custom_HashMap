56066ca105dfcd5ffc993edebc985c07e60d0595
HalilCan <halilcanmemoglu@gmail.com>
2017-09-29 14:39:36 -0400

    Extend demo

--- src/com/halilcanm/Main.java
+++ src/com/halilcanm/Main.java
@@ -5,17 +5,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Demo with nine different 'sets' with one overwrite");
         System.out.println();
-        HashMap<Integer, Integer> hMap = new HashMap<Integer, Integer>(4);
+        HashMap<String, Integer> hMap = new HashMap<String, Integer>(4);
 
-        hMap.set(10, 55);
-        hMap.set(12, 52);
-        hMap.set(16, 56);
-        hMap.set(10, 50);
-        hMap.set(13, 53);
-        hMap.set(14, 54);
-        hMap.set(15, 55);
-        hMap.set(11, 51);
-        hMap.set(23, 63);
+        hMap.set("ten", 55);
+        hMap.set("quick", 52);
+        hMap.set("brown", 56);
+        hMap.set("fox", 50);
+        hMap.set("jumped", 53);
+        hMap.set("over", 54);
+        hMap.set("the", 55);
+        hMap.set("lazy", 51);
+        hMap.set("dog", 63);
 
         hMap.quickDraw();
     }
